951d251eec23c4f23a2459712845561cd6c7c55a
mohsetiawanmodeong <mohsetiawanmodeong@gmail.com>
2022-05-24 13:46:47 +0800

    add footer

--- src/elements/Button.js
+++ src/elements/Button.js
@@ -2,18 +2,21 @@ import React from "react";
 import propTypes from 'prop-types'
 
 export default function Button(props) {
-    const className = ['button']
-    if (props.isPrimary)
-        className.push("button-primary")
-    if (props.isWideMobile)
-        className.push("button-wide-mobile")
-    if (props.isSmall)
-        className.push("button-sm")
-    return <button className={className.join(" ")}>{props.children}</button>;
+	const className = ['button']
+	if (props.isPrimary)
+		className.push("button-primary")
+	if (props.isWideMobile)
+		className.push("button-wide-mobile")
+	if (props.isBlock)
+		className.push("button-block")
+	if (props.isSmall)
+		className.push("button-sm")
+	return <button className={className.join(" ")}>{props.children}</button>;
 }
 
-Button.PropTypes = {
-    isPrimary: propTypes.bool,
-    isWideMobile: propTypes.bool,
-    isSmall: propTypes.bool
+Button.propTypes = {
+	isPrimary: propTypes.bool,
+	isWideMobile: propTypes.bool,
+	isBlock: propTypes.bool,
+	isSmall: propTypes.bool
 }
--- src/parts/Clients.js
+++ src/parts/Clients.js
@@ -9,31 +9,31 @@ import Client04 from "assets/images/clients-04.svg";
 import Client05 from "assets/images/clients-05.svg";
 
 export default function Clients() {
-    return (
-        <Section className="clients">
-            <Fade bottom delay={2000}>
-                <div className="container">
-                    <div className="clients-inner section-inner has-top-divider has-bottom-divider">
-                        <ul className="list-reset">
-                            <li>
-                                <img src={Client01} alt="Clients 01" />
-                            </li>
-                            <li>
-                                <img src={Client02} alt="Clients 02" />
-                            </li>
-                            <li>
-                                <img src={Client03} alt="Clients 03" />
-                            </li>
-                            <li>
-                                <img src={Client04} alt="Clients 04" />
-                            </li>
-                            <li>
-                                <img src={Client05} alt="Clients 05" />
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </Fade>
-        </Section>
-    )
+   return (
+      <Section className="clients">
+         <Fade bottom delay={2000}>
+            <div className="container">
+               <div className="clients-inner section-inner has-top-divider has-bottom-divider">
+                  <ul className="list-reset">
+                     <li>
+                        <img src={Client01} alt="Clients 01" />
+                     </li>
+                     <li>
+                        <img src={Client02} alt="Clients 02" />
+                     </li>
+                     <li>
+                        <img src={Client03} alt="Clients 03" />
+                     </li>
+                     <li>
+                        <img src={Client04} alt="Clients 04" />
+                     </li>
+                     <li>
+                        <img src={Client05} alt="Clients 05" />
+                     </li>
+                  </ul>
+               </div>
+            </div>
+         </Fade>
+      </Section>
+   )
 }
